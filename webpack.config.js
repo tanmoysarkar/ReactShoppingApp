6b2b4840269636751e9c89155948f05e1be6a7f0
Tanmoy Sarkar <tnmsarkar96@gmail.com>
2018-01-15 00:52:21 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
           }
        },
        {     test: /\.css$/,
-          loaders: ['style-loader', 'css-loader']
+              loader:'style!css!'
 
         },
        {
